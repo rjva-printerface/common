049c1ddf6052cdad5ac873f792f8cec76c9bfa51
Rajeev van Appeldorn <rajeev.vanappeldorn@printerface.io>
2022-07-26 17:26:33 +0200

    feat(client): mongo client update

--- client/mongo.go
+++ client/mongo.go
@@ -27,6 +27,8 @@ func CreateMongoClient() (mc *mongo.Client, cancel context.CancelFunc) {
 	log := helpers.NewLog("mongo")
 	mongoUri := getMongoUri()
 
+	log.Print(mongoUri, colors.Green)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
 
